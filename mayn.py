f36ca93c9e819564f1ab4b50fa97d0aad20a91fb
Kosya Mondok <kostya.mondok.2019.1@gmail.com>
2026-07-12 14:51:56 +0200

    random

--- mayn.py
+++ mayn.py
@@ -1,32 +1,33 @@
+import random
 import time
 from questions import QUIZ_DATA
 
+questions_pool = QUIZ_DATA[:]
+
 print("ЛАСКАВО ПРОСИМО ДО ІНТЕЛЕКТ-КВІЗУ")
 print("Перед вами 10 питань. Натискайте А, Б, В або Г для відповіді.")
-print("Починаємо через 2 години")
-time.sleep(1)
+hours, minutes, second = map(int, input("Введіть час: год, хв, с (через пробіл.)").split())
+final_time = (hours * 3600 + minutes * 60 + second)
+time.sleep(hours * 3600 + minutes * 60 + second)
 
 total_score = 0
 score = 0
-total_questions = len(QUIZ_DATA)
-for index, item in enumerate(QUIZ_DATA):
-    print(f"\n Питання №{index + 1}: {item.get("question")}")
+
+while len(questions_pool) > 0:
+    random_index = random.randint(0, len(questions_pool) - 1)
+    item = questions_pool.pop(random_index)
+    print(f"\n Питання: {item.get('question') or item.get('qestion')}")
     for option in item["options"]:
         print(f"  {option}")
     user_answer = input("\nВаша відповідь (А, Б, В, Г): ").strip().upper()
-    if user_answer == item.get("answer"):
+    correct_answer = item.get("answer") or item.get("ansver")
+    if user_answer == correct_answer:
         print(" Правильно! + ", item.get("skore"), " бал")
         score += item.get("skore")
-        total_score += item.get("skore")
-
     else:
-        print(f" Неправильно. Правильна відповідь: {item.get("answer")}")
-        total_score += item.get("skore")
+        print(f" Неправильно. Правильна відповідь: {correct_answer}")
+    total_score += item.get("skore")
+    time.sleep(1)
 
-    time.sleep(2)
-
-print("РЕЗУЛЬТАТИ ТЕСТУВАННЯ")
-print(f"• Балів: {score} з {total_score}")
-
-success_rate = (score / total_score) * 100
-print (success_rate)
+print(f"\nРЕЗУЛЬТАТ: {score} з {total_score} балів.")
+print(score / total_score * 100)
